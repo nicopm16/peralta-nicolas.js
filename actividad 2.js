47c6a3deb4b60beeae26d4231c1e3d8f65410edc
nicopm16 <nicolasperaltam1121@gmail.com>
2024-11-15 09:12:41 -0300

    Actividad 2 (JS)

---
+++ actividad 2.js
@@ -0,0 +1,11 @@
+// ingresar el primer número
+const numero1 = parseFloat(prompt("Ingresa el primer número:"));
+
+// ingresar el segundo número
+const numero2 = parseFloat(prompt("Ingresa el segundo número:"));
+
+// Crear una tercera variable que sume los valores
+const suma = numero1 + numero2;
+
+// Mostrar el resultado
+console.log("La suma de los dos números es:", suma);
